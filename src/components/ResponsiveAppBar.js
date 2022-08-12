579929c000c8e573fa248cc320754a3c9a80c45c
overproff <fcaravacacrespo+over@gmail.com>
2022-08-12 10:15:27 +0200

    better navbar

--- src/components/ResponsiveAppBar.js
+++ src/components/ResponsiveAppBar.js
@@ -10,8 +10,10 @@ import Container from '@mui/material/Container';
 
 import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
-
+import Stack from '@mui/material/Stack';
 import MenuItem from '@mui/material/MenuItem';
+import Divider from '@mui/material/Divider';
+import Avatar from '@mui/material/Avatar';
 
 
 const pages = ['Downloads', 'Privacy Policy'];
@@ -19,92 +21,109 @@ const pages_url = {'Downloads': '/downloads', 'Privacy Policy': '/privacy_policy
 
 const ResponsiveAppBar = () => {
 
-  return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component={Link}
-            to="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-              OVERPROFILING
-          </Typography>
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              color="inherit"
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="sticky">
+    <Container maxWidth="xl">
+        <Toolbar disableGutters sx={{minHeight: "68.5px"}}>
+        <Stack direction="row" spacing={1} alignItems="center" sx={{display: { xs: 'none', md: 'flex' }}}>
+            <Avatar variant="rounded" src="/logo_128.png" alt="logo" component={Link} to={"/"}/>
+            <Typography
+                variant="h6"
+                noWrap
+                component={Link} to={"/"}
+                sx={{
+                mr: 2,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+                alignItems: 'center'
+                }}
             >
-              <MenuIcon />
+                OVERPROFILING
+            </Typography>
+        </Stack>
+
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            color="inherit"
+            onClick={handleOpenNavMenu}
+            >
+            <MenuIcon />
             </IconButton>
             <Menu
-              id="menu-appbar"
-              anchorOrigin={{
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            open={Boolean(anchorElNav)}
+            anchorOrigin={{
                 vertical: 'bottom',
                 horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
+            }}
+            keepMounted
+            transformOrigin={{
                 vertical: 'top',
                 horizontal: 'left',
-              }}
-              sx={{
+            }}
+            onClose={handleCloseNavMenu}
+            sx={{
                 display: { xs: 'block', md: 'none' },
-              }}
+            }}
             >
-              {pages.map((page) => (
-                <MenuItem key={page}>
-                  <Typography textAlign="center" style={{color: "white"}} component={Link} to={pages_url[page]}>{page}</Typography>
+            {pages.map((page) => (
+                <MenuItem key={page} onClick={() => { handleCloseNavMenu()}} component={Link} to={pages_url[page]}>
+                <Typography textAlign="center" style={{color: "white"}} >{page}</Typography>
                 </MenuItem>
-              ))}
+            ))}
             </Menu>
-          </Box>
-          <Typography
+        </Box>
+        <Typography
             variant="h5"
             noWrap
-            component={Link}
-            to=""
             sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+            mr: 2,
+            display: { xs: 'flex', md: 'none' },
+            flexGrow: 1,
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+            color: 'inherit',
+            textDecoration: 'none',
             }}
-          >
+        >
             OVERPROFILING
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+        </Typography>
+        <Box sx={{ flexGrow: 1, mx: 1, display: { xs: 'none', md: 'flex' }}}>
+            <Stack direction="row" spacing={1} divider={<Divider orientation="vertical" flexItem variant="middle"/>}>
+            
             {pages.map((page) => (
-              <Button
+            <Button
                 key={page}
-                component={Link} to={pages_url[page]}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
+                sx={{  color: 'white', display: 'block' }}
+                component={Link}
+                to={pages_url[page]}
+            >
                 {page}
-              </Button>
+            </Button>
             ))}
-          </Box>
+            </Stack>
+        </Box>
 
         </Toolbar>
-      </Container>
+    </Container>
     </AppBar>
   );
 };
