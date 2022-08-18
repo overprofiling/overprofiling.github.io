6f8e07e276aa80db5967493af1f1991406bb5a4d
overproff <fcaravacacrespo+over@gmail.com>
2022-08-18 13:02:12 +0200

    Updated policy

--- src/routes/Policy.js
+++ src/routes/Policy.js
@@ -22,17 +22,16 @@ export default function Policy() {
         <AccordionDetails>
           <Typography>
           <h3>Purpose</h3>
-      <p>This browser extension is aimed to collect information about Ads, Ads interests and user behavior  regarding ads. The information collected will
-      be used for researching purposes only, and not for commercial usage.
-      </p>
-      <h3>Data collected and Data handling</h3>
-      <p>This is the information that we get access.</p>
+      <p>The goal of this research project is willing to understand how accurate are the profiles Big. Tech companies create from users. In particular, we focus on the following companies: Facebook, Twitter, LinkedIn and Google. The way these companies operate is creating a profile for a user based on the activity of this user assigning them demographic information (e.g., gender, age, location, etc,), interests (e.g., soccer, music, Italian food, etc.) and other elements. The referred companies allow advertisers to run ad campaigns targeting specific profiles defined by the advertiser. Our research projects aims to use advance research techniques to quantify how accurate the profiles created by Big. Tech companies are. This will allow us answering questions like: Do BigTech companies assign users an reasonable number of interests or instead they assign a very large number of interests beyond what an actual human being can cope with? The interests assigned are actually relevant for the users or in fact they are not aligned to what users are interested in? etc. </p>
+      <p>To run this project we will have to collect the interests the four referred companies assign to users. To this end we have developed a web browser add on that implements this task. The web browser add on will operate only upon the user provide an explicit consent by accepting these terms of use. </p>
+      <p>The data collected for this of this research project is limited to research purposes and will not be used for commercial purposes.</p>
+      
+      <h3>Data collected and Processing</h3>
+      <p>The main piece of data we collect are the interests assigned to the users. While our intention is not collecting personal data, we understand that the combination of many interests together may make a user unique in a given platform (e.g., Facebook) and then we have adopted a conservative approach and manage that information as personal data.
+This is the main reason why we ask you an explicit consentwhen installing the browser add on.</p>
+      <p>Following, we list the information we collect:</p>
       <ul>
-        <li><b>Interests: </b><span>We collect interests</span> associated with your
-          Facebook, Twitter, LinkedIn and Google profiles. We collect this
-          information through the Ad Preference Managers
-          (APMs) of each platform every 30 minutes. These are the links
-        from where we collect the information: </li>
+        <li><b>Interests:</b> We collect interests associated with your Facebook, Twitter, LinkedIn and Google profiles. We collect this information through the Ad Preference Managers (APMs) of each platform every 30 minutes. These are the links from where we collect the information:</li>
           <p><span>Facebook:</span>  <a href="https://m.facebook.com/ads/preferences/categories/"  rel="noreferrer" target="_blank">https://m.facebook.com/ads/preferences/categories/</a></p>
           <p><span>Twitter:</span> <a href="https://twitter.com/settings/your_twitter_data/twitter_interests"  rel="noreferrer" target="_blank">https://twitter.com/settings/your_twitter_data/twitter_interests</a> </p>
           <p><span>LinkedIn:</span>  <a href="https://www.linkedin.com/psettings/advertising/li-enterprise-product"  rel="noreferrer" target="_blank">https://www.linkedin.com/psettings/advertising/li-enterprise-product</a> </p>
@@ -43,7 +42,7 @@ export default function Policy() {
         <li><b>Ads:</b> we collect the Ads delivered to you on Facebook, Twitter, LinkedIn, YouTube and general websites.
           This include: IDs of the ad, post, images and text associated to them, author of the ad and landing page. 
           We also collect the explanations of why you received a specific ad.
-          In the case of Google ads, will we also collect the HTML of the ad and in which URL the ad was shown to you. In YouTube video ads are not collected.</li>
+          In the case of Google ads, will we also collect the HTML of the ad and in which URL the ad was shown to you. In YouTube, video ads are not collected.</li>
 
         <li><b>Clicks on ads:</b> the extension will also collect whether you have clicked an ad or visited the landing page of any ad.</li>
 
@@ -56,13 +55,15 @@ export default function Policy() {
       </ul>
       <p>The data is accessed by inspecting the HTML of pages you use, analyzing HTTPS request and responses or making HTTPS requests using your browser to the different platforms (Twitter, Linkedin, Facebook, Google)</p>
 
-      <p>The information collected is also shown to the user, using the extension interface. There are different charts available in which some summarised data is shown.</p>
+      <p>The browser add on allows the user to visualise the collected information through the browser add on interface. </p>
 
-      <p>  We do not collect any Personally Identifiable Information
-        (PII). To distinguish you between all the users,
-        we generate an ID of 128 bits created once you install the extension and accept these terms.
-        This ID and the information collected remain stored on the browser
-        until you remove the extension.</p>
+      <p>We create a user ID generating a random 128 bit string that is created upon installing the browser add on and accepting these terms of use. This ID and the information collected remain stored on the browser until you remove the extension.</p>
+      <p>
+      In addition, we also store the collected information in a backend server located at Universidad Carlos III de Madrid (UC3M) premises. This server is protected by advanced security measures provided by the IT services to protect the Universidad Carlos III de Madrid network. In addition, the server is behind a second local firewall. The server cannot only be accessed locally from the UC3M network or the VPN offered by UC3M. The access to the server is limited to the research team participating in the project under credentials protected by user and password. Similarly, the access to the database storing the information is limited to the research team participating in the project and is also protected by user and password credentials.
+      </p>
+      <p>
+      Finally, the information will be stored in the server for the purpose of the research project and will be removed 2 months after the publication of the final research article derived from this project. 
+      </p>
           </Typography>
         </AccordionDetails>
 
